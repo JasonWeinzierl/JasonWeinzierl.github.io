aca9241e93a62ff35f2e7889e6b63b843e9bfb68
Jason Weinzierl <weinzierljason@gmail.com>
2018-02-12 21:46:17 -0600

    add project content

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -3,13 +3,13 @@ function filterContent(contentID) {
     document.getElementById("ajax-loading").style.opacity = 1;
 
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             // set content to xmlhttp's response
             document.getElementById("content").innerHTML = this.responseText;
 
             // done!
-            setTimeout(function(){document.getElementById("ajax-loading").style.opacity = 0;}, 50);
+            setTimeout(function(){document.getElementById("ajax-loading").style.opacity = 0; }, 50);
         }
     }
 
