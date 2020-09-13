74e541b3d5af35a59b977c773f242dd5ea3bb53d
Jason Weinzierl <weinzierljason@gmail.com>
2020-09-12 22:35:16 -0500

    download error docs from existing site and new favicon

---
+++ errors/notfound.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html lang="en">
+    <head>
+        <title>404 File Not Found</title>
+		<meta charset="UTF-8">
+    </head>
+    <body>
+        <main>
+            <h1>404 File Not Found</h1>
+            <p>The requested URL <b><?php echo $_SERVER[REQUEST_URI]; ?></b> was not found on this server.</p>
+            <p>Please return to our <a href="/">Home</a>.  We apologize for the inconvenience.</p>
+            <hr>
+        </main>
+    </body>
+</html>
